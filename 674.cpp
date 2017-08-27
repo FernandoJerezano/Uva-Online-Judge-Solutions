ab81153707104b929fa319a74055ee6a130d9d84
FernandoJerezano <fernando._jerezano@hotmail.com>
2017-08-27 15:07:06 -0500

    Create 674.cpp

---
+++ 674.cpp
@@ -0,0 +1,44 @@
+// Problema:    674 - Coin Change
+// Autor:       FernandoJerezano
+// Fecha:       27-Agosto-2017
+#include <iostream>
+#include <bits/stdc++.h>
+#define MAX 8001
+
+using namespace std;
+
+int ways[MAX];
+
+void count_ways(){
+
+    int cents[] = {1,5,10,25,50};
+    int cs = sizeof(cents)/sizeof(cents[0]);
+
+    for(int i=0;i<MAX;i++){
+        ways[i] = 0;
+    }
+
+    ways[0] = 1;
+
+    for(int i=0;i<cs;i++){
+        for(int j=cents[i];j<=MAX;j++){
+            ways[j] += ways[j-cents[i]];
+        }
+    }
+}
+
+int main(){
+
+    //freopen("entrada.txt","r",stdin);
+    //freopen("salida.txt","w",stdout);
+
+    int cents;
+
+    count_ways();
+    while(scanf("%d",&cents)!=EOF){
+        printf("%d\n",ways[cents]);
+
+    }
+    return 0;
+}
+
